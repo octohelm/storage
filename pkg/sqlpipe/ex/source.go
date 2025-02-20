eb8d17235a0ae968f0bc76bfc7043e920cb766e5
Morlay <morlay.null@gmail.com>
2025-02-20 11:33:57 +0800

    docs: dock on sqlpipe/ex.SourceExecutor

--- pkg/sqlpipe/ex/source.go
+++ pkg/sqlpipe/ex/source.go
@@ -19,32 +19,31 @@ import (
 type SourceExecutor[M sqlpipe.Model] interface {
 	sqlpipe.Source[M]
 
+	// From to load data source other source for inserting
 	From(source sqlpipe.Source[M]) SourceExecutor[M]
-
+	// PipeE compose operators as SourceExecutor
 	PipeE(operators ...sqlpipe.SourceOperator[M]) SourceExecutor[M]
-
 	// Commit just execute
 	Commit(ctx context.Context) error
 
-	// Items execute and return item or error
+	// Items execute and return Model or error
 	Items(ctx context.Context) iter.Seq2[*M, error]
-
-	// Item deprecated use Items instead
-	Item(ctx context.Context) iter.Seq2[*M, error]
-
-	// Range execute then range
-	Range(ctx context.Context, fn func(*M)) error
-
-	// FindOne execute then scan as one item
+	// FindOne execute then scan as Model or error
 	// notice this will return nil when not result found
 	FindOne(ctx context.Context) (*M, error)
 	// List execute then list as item list
 	List(ctx context.Context) ([]*M, error)
-
 	// ListTo execute then list to item adder
 	ListTo(ctx context.Context, adder Adder[M]) error
 	// CountTo execute as count
 	CountTo(ctx context.Context, x *int64) error
+
+	// Item
+	// Deprecated use Items instead
+	Item(ctx context.Context) iter.Seq2[*M, error]
+	// Range execute then range
+	// Deprecated use Items instead
+	Range(ctx context.Context, fn func(*M)) error
 }
 
 type Adder[M sqlpipe.Model] interface {
@@ -62,33 +61,46 @@ type Executor[M sqlpipe.Model] struct {
 	operators []sqlpipe.SourceOperator[M]
 }
 
-func (e *Executor[M]) Tx(ctx context.Context, do func(ctx context.Context) error) error {
-	return e.session(ctx).Tx(ctx, do)
-}
-
-func (e *Executor[M]) From(src sqlpipe.Source[M]) SourceExecutor[M] {
-	return FromSource(src)
-}
-
-func (e *Executor[M]) Pipe(operators ...sqlpipe.SourceOperator[M]) sqlpipe.Source[M] {
-	return e.source().Pipe(operators...)
-}
-
+// IsNil if true will omit in sql building
 func (e *Executor[M]) IsNil() bool {
 	return e.source().IsNil()
 }
 
+// Frag  for sql building
 func (e *Executor[M]) Frag(ctx context.Context) iter.Seq2[string, []any] {
 	return e.source().Frag(ctx)
 }
 
+// ApplyStmt for Builder
 func (e *Executor[M]) ApplyStmt(ctx context.Context, s *internal.Builder[M]) *internal.Builder[M] {
 	return e.source().ApplyStmt(ctx, s)
 }
 
-func (e Executor[M]) PipeE(operators ...sqlpipe.SourceOperator[M]) SourceExecutor[M] {
-	e.operators = append(e.operators, operators...)
-	return &e
+// Tx begin transaction if not in some transaction
+func (e *Executor[M]) Tx(ctx context.Context, do func(ctx context.Context) error) error {
+	return e.session(ctx).Tx(ctx, do)
+}
+
+// From to load data source other source for inserting
+func (e *Executor[M]) From(src sqlpipe.Source[M]) SourceExecutor[M] {
+	return FromSource(src)
+}
+
+// Pipe compose operators as Source
+func (e *Executor[M]) Pipe(operators ...sqlpipe.SourceOperator[M]) sqlpipe.Source[M] {
+	return e.source().Pipe(operators...)
+}
+
+// PipeE compose operators as SourceExecutor
+func (e *Executor[M]) PipeE(operators ...sqlpipe.SourceOperator[M]) SourceExecutor[M] {
+	if operators == nil {
+		return e
+	}
+
+	return &Executor[M]{
+		src:       e.src,
+		operators: slices.Concat(e.operators, operators),
+	}
 }
 
 func (e *Executor[M]) session(ctx context.Context) session.Session {
@@ -162,44 +174,13 @@ func flatten[M sqlpipe.Model](opSeq iter.Seq[sqlpipe.SourceOperator[M]]) iter.Se
 	}
 }
 
+// Commit execute sql
 func (e *Executor[M]) Commit(ctx context.Context) error {
 	_, err := e.session(ctx).Adapter().Exec(ctx, e.source())
 	return err
 }
 
-func (e *Executor[M]) List(ctx context.Context) ([]*M, error) {
-	list := make([]*M, 0)
-
-	for item, err := range e.Items(ctx) {
-		if err != nil {
-			return nil, err
-		}
-		list = append(list, item)
-	}
-
-	return list, nil
-}
-
-func (e *Executor[M]) ListTo(ctx context.Context, listAdder Adder[M]) error {
-	for item, err := range e.Items(ctx) {
-		if err != nil {
-			return err
-		}
-		listAdder.Add(item)
-	}
-	return nil
-}
-
-func (e *Executor[M]) FindOne(ctx context.Context) (*M, error) {
-	for item, err := range e.PipeE(sqlpipe.Limit[M](1)).Item(ctx) {
-		if err != nil {
-			return nil, err
-		}
-		return item, err
-	}
-	return nil, nil
-}
-
+// Items execute sql and returns Model or error as iter.Seq
 func (e *Executor[M]) Items(ctx context.Context) iter.Seq2[*M, error] {
 	s := e.session(ctx)
 
@@ -218,6 +199,21 @@ func (e *Executor[M]) Items(ctx context.Context) iter.Seq2[*M, error] {
 	return x.Items(ctx)
 }
 
+// List execute sql and return Model slice
+func (e *Executor[M]) List(ctx context.Context) ([]*M, error) {
+	list := make([]*M, 0)
+
+	for item, err := range e.Items(ctx) {
+		if err != nil {
+			return nil, err
+		}
+		list = append(list, item)
+	}
+
+	return list, nil
+}
+
+// CountTo execute sql for count and marshal value into int64 ptr
 func (e *Executor[M]) CountTo(ctx context.Context, x *int64) error {
 	s := e.session(ctx)
 
@@ -233,10 +229,37 @@ func (e *Executor[M]) CountTo(ctx context.Context, x *int64) error {
 	return scanner.Scan(ctx, rows, x)
 }
 
+// ListTo execute sql and return add Model into some Adder
+func (e *Executor[M]) ListTo(ctx context.Context, listAdder Adder[M]) error {
+	for item, err := range e.Items(ctx) {
+		if err != nil {
+			return err
+		}
+		listAdder.Add(item)
+	}
+	return nil
+}
+
+// FindOne execute then scan as Model or error
+// notice this will return nil when not result found
+func (e *Executor[M]) FindOne(ctx context.Context) (*M, error) {
+	for item, err := range e.PipeE(sqlpipe.Limit[M](1)).Item(ctx) {
+		if err != nil {
+			return nil, err
+		}
+		return item, err
+	}
+	return nil, nil
+}
+
+// Item
+// Deprecated use Items instead
 func (e *Executor[M]) Item(ctx context.Context) iter.Seq2[*M, error] {
 	return e.Items(ctx)
 }
 
+// Range
+// Deprecated use Items instead
 func (e *Executor[M]) Range(ctx context.Context, fn func(m *M)) error {
 	for item, err := range e.Items(ctx) {
 		if err != nil {
